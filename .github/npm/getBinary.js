119ee472a82f6bb9b5aac68e43bfd541bd451e14
Zdenek Hrebicek <zdenek@coralogix.com>
2022-05-18 10:15:48 +0200

    (cargo-release) version 0.0.13

--- .github/npm/getBinary.js
+++ .github/npm/getBinary.js
@@ -5,24 +5,20 @@ function getPlatform() {
 	const type = os.type();
 	const arch = os.arch();
 
-	if (type === 'Windows_NT') {
-		if (arch === 'x64') {
-			return 'win64';
-		} else {
-			return 'win32';
-		}
+	if (type === 'Windows_NT' && arch === 'x64') {
+		return 'windows_amd64';
 	}
 
 	if (type === 'Linux' && arch === 'x64') {
-		return 'linux';
+		return 'linux_amd64';
 	}
 
 	if (type === 'Darwin' && arch === 'x64') {
-		return 'macos-amd64';
+		return 'darwin_amd64';
 	}
 
 	if (type === 'Darwin' && arch === 'arm64') {
-		return 'macos-arm64';
+		return 'darwin_arm64';
 	}
 
 	throw new Error(`Unsupported platform: ${type} ${arch}. Please create an issue at https://github.com/coralogix/protofetch/issues`);
@@ -31,7 +27,7 @@ function getPlatform() {
 function getBinary() {
 	const platform = getPlatform();
 	const version = require('../package.json').version;
-	const url = `https://github.com/coralogix/protofetch/releases/download/v${ version }/protofetch-${ platform }.tar.gz`;
+	const url = `https://github.com/coralogix/protofetch/releases/download/v${ version }/protofetch_${ platform }.tar.gz`;
 	return new Binary(url, { name: 'protofetch' });
 }
 
