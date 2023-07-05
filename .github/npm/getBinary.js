3ac26501c4449d2d21cfdd9b3644857d2dc7b376
Roman Timushev <roman.timushev@coralogix.com>
2023-07-05 15:58:22 +0200

    Do not pretend like we have an aarch64-darwin build

--- .github/npm/getBinary.js
+++ .github/npm/getBinary.js
@@ -22,7 +22,8 @@ function getPlatform() {
 	}
 
 	if (type === 'Darwin' && arch === 'arm64') {
-		return 'darwin_arm64';
+		// This requires Rosetta
+		return 'darwin_amd64';
 	}
 
 	throw new Error(`Unsupported platform: ${type} ${arch}. Please create an issue at https://github.com/coralogix/protofetch/issues`);
@@ -31,7 +32,7 @@ function getPlatform() {
 function getBinary() {
 	const platform = getPlatform();
 	const version = require('./package.json').version;
-	const url = `https://github.com/coralogix/protofetch/releases/download/v${ version }/protofetch_${ platform }.tar.gz`;
+	const url = `https://github.com/coralogix/protofetch/releases/download/v${version}/protofetch_${platform}.tar.gz`;
 	const name = 'protofetch';
 
 	return new Binary(name, url)
