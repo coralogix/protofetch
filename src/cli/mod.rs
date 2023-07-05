2008625bb2973ad619b8a80a0e2c3799e915b8bb
Roman Timushev <roman.timushev@coralogix.com>
2023-07-05 11:51:08 +0200

    Use the correct auth type depending on the actual protocol (#62)
    * Do not require HTTP auth
    * Use the correct auth type depending on the actual protocol

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,6 +1,6 @@
 use derive_new::new;
 use git2::Config;
-use std::{env, error::Error};
+use std::env;
 
 pub mod args;
 pub mod command_handlers;
@@ -21,33 +21,19 @@ impl HttpGitAuth {
     /// If 2FA is enabled please generate an access token and use it as password. Please see:
     /// https://github.blog/2013-09-03-two-factor-authentication/#how-does-it-work-for-command-line-git
     pub fn resolve_git_auth(
+        config: &Config,
         cli_username: Option<String>,
         cli_password: Option<String>,
-    ) -> Result<Option<HttpGitAuth>, Box<dyn Error>> {
-        let cfg = Config::open_default()?;
+    ) -> Option<HttpGitAuth> {
         let username = cli_username
             .or_else(|| env::var("GIT_USERNAME").ok())
-            .or_else(|| cfg.get_string("user.name").ok());
+            .or_else(|| config.get_string("user.name").ok());
         let password = cli_password
             .or_else(|| env::var("GIT_PASSWORD").ok())
-            .or_else(|| cfg.get_string("user.password").ok());
+            .or_else(|| config.get_string("user.password").ok());
         match (username, password) {
-            (Some(username), Some(password)) => Ok(Some(HttpGitAuth { username, password })),
-            (Some(username), None) => {
-                warn!("Git user {} found but no password found for git auth, which is used for fetching via https. \
-                 If you are using https please pass as command line, GIT_PASSWORD env variable or add to git config.", username);
-                Ok(None)
-            }
-            (None, Some(password)) => {
-                warn!("Git password {} found but no user found for git auth, which is used for fetching via https. \
-                If you are using https please pass as command line, GIT_USERNAME env variable or add to git config.", password);
-                Ok(None)
-            }
-            _ => {
-                warn!("No git auth found, used for fetching via https. If you are using https please pass as \
-                command line parameters, GIT_USERNAME and GIT_PASSWORD env variables or add to git config.");
-                Ok(None)
-            }
+            (Some(username), Some(password)) => Some(HttpGitAuth { username, password }),
+            _ => None,
         }
     }
 }
