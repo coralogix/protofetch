coralogix/protofetch

commit 7b741d3838661276f8e1ac3ae63ae25a05b65b76

Update dependencies (#144)
1. Support broader version range for git2. Unlike regular dependencies,
for the native ones cargo refuses to have different versions of
libgit2-sys in the dependency tree, so it's better to be more permissive
here.
2. Check that protofetch compiles with MSRV and the minimal dependency
versions from Cargo.toml.
3. Use more recent toolchain by default.

.github/workflows/ci.yml
@@ -47,6 +47,23 @@ jobs:
       - name: Run cargo test
         run: cargo test
 
+  versions:
+    runs-on: ubuntu-latest 
+    steps:
+      - uses: taiki-e/install-action@v2
+        with:
+          tool: cargo-hack,cargo-minimal-versions
+
+      - name: Checkout sources
+        uses: actions/checkout@v3
+
+      - name: Cache rust dependencies
+        uses: Swatinem/rust-cache@v2
+
+      # Check with minimal dependency versions and MSRV from Cargo.toml
+      - name: Check minimal versions
+        run: cargo minimal-versions check --rust-version --features vendored-openssl,vendored-libgit2
+
   semver:
     runs-on: ubuntu-latest 
     steps:
@@ -65,7 +82,7 @@ jobs:
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 
   package:
-    needs: [ lint, test, semver ]
+    needs: [ lint, test, versions, semver ]
     strategy:
       fail-fast: false
       matrix:

Cargo.toml
@@ -2,6 +2,7 @@
 name = "protofetch"
 version = "0.1.6"
 edition = "2021"
+rust-version = "1.74"
 license = "Apache-2.0"
 description = "A source dependency management tool for Protobuf."
 homepage = "https://github.com/coralogix/protofetch"
@@ -16,20 +17,20 @@ vendored-openssl = ["git2/vendored-openssl"]
 vendored-libgit2 = ["git2/vendored-libgit2"]
 
 [dependencies]
-anyhow = "1.0.75"
-clap = { version = "4.4.7", features = ["derive"] }
-config = { version = "0.13.3", default-features = false, features = ["toml"] }
-env_logger = { version = "0.10.0", default-features = false, features = ["auto-color"] }
-git2 = "0.18.1"
+anyhow = "1.0.86"
+clap = { version = "4.5.9", features = ["derive"] }
+config = { version = "0.14.0", default-features = false, features = ["toml"] }
+env_logger = { version = "0.11.3", default-features = false, features = ["auto-color"] }
+git2 = ">=0.18.0, <0.20.0"
 gix-lock = { version = "14.0.0" }
 gix-tempfile = { version = "14.0.0", features = ["signals"] }
-home = "0.5.5"
-log = "0.4.20"
-regex-lite = "0.1.5"
-serde = { version = "1.0.190", features = ["derive"] }
-ssh-key = "0.6.2"
-thiserror = "1.0.50"
-toml = { version = "0.8.6", features = ["preserve_order"] }
+home = "0.5.9"
+log = "0.4.22"
+regex-lite = "0.1.6"
+serde = { version = "1.0.204", features = ["derive"] }
+ssh-key = "0.6.6"
+thiserror = "1.0.62"
+toml = { version = "0.8.14", features = ["preserve_order"] }
 
 [dev-dependencies]
 pretty_assertions = "1.4.0"

rust-toolchain.toml
@@ -2,4 +2,4 @@
 # The default profile includes rustc, rust-std, cargo, rust-docs, rustfmt and clippy.
 # https://rust-lang.github.io/rustup/concepts/profiles.html
 profile = "default"
-channel = "1.70.0"
+channel = "1.79.0"

src/main.rs
@@ -73,18 +73,17 @@ fn main() {
             let level = record.level();
             let style = buf.default_level_style(level);
 
+            write!(buf, "{style}{}{style:#}", level)?;
+
             if at_least_debug_log {
-                writeln!(
+                write!(
                     buf,
-                    "{} [{}:{}] {}",
-                    style.value(level),
+                    " [{}:{}]",
                     record.file().unwrap_or("unknown"),
                     record.line().unwrap_or(0),
-                    record.args()
-                )
-            } else {
-                writeln!(buf, "{} {}", style.value(level), record.args())
+                )?;
             }
+            writeln!(buf, " {}", record.args())
         })
         .init();
 

src/model/protofetch/mod.rs
@@ -294,8 +294,8 @@ impl DenyPolicies {
             let filtered = FilePolicy::apply_file_policies(&deny_policies.policies, files);
             files
                 .iter()
-                .cloned()
                 .filter(|f| !filtered.contains(f))
+                .cloned()
                 .collect()
         }
     }

commit 98685232ad6c0d47aa78623291c9efd80d1306af

Fix nix flake build and check this on CI (#145)
Broken by https://github.com/coralogix/protofetch/pull/144, which made
dependencies incompatible with the pinned rust toolchain.

This PR will check Nix Flake build on CI, but won't block PR merges if
the check fails.

.github/workflows/ci.yml
@@ -72,6 +72,21 @@ jobs:
 
       - name: Check semver
         uses: obi1kenobi/cargo-semver-checks-action@v2
+  
+  flake:
+    runs-on: ubuntu-latest 
+    steps:
+      - name: Checkout sources
+        uses: actions/checkout@v3
+
+      - name: Install nix
+        uses: DeterminateSystems/nix-installer-action@v12
+
+      - name: Cache nix dependencies
+        uses: DeterminateSystems/magic-nix-cache-action@v7
+
+      - name: Build
+        run: nix flake check
 
   update-release-draft:
     runs-on: ubuntu-latest

flake.nix
@@ -1,41 +1,54 @@
 {
-    description = "Protofetch - A source dependency management tool for Protobuf files";
+  description = "Protofetch - A source dependency management tool for Protobuf files";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-        crane = { url = "github:ipetkov/crane"; inputs.nixpkgs.follows = "nixpkgs"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, flake-utils, crane }: flake-utils.lib.eachDefaultSystem (system:
-    let
-        pkgs = import nixpkgs { inherit system; };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      crane,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib;
-        craneLib = crane.lib.${system};
-    in {
-        packages = rec {
-            default = protofetch;
-            protofetch = craneLib.buildPackage {
-                pname = "protofetch";
-                src = lib.cleanSourceWith {
-                    src = ./.; # The original, unfiltered source
-                    filter = path: type:
-                        (lib.hasSuffix "\.proto" path) ||
-                        (craneLib.filterCargoSources path type);
-                };
-                buildInputs = with pkgs; [
-                    openssl
-                    libgit2
-                    pkg-config
-                ] ++ (if stdenv.isDarwin then [darwin.apple_sdk.frameworks.Security] else []);
-                preBuild = ''
-                    export HOME=$(mktemp -d)
-                '';
-            };
+        craneLib = crane.mkLib pkgs;
+
+        protofetch = craneLib.buildPackage {
+          pname = "protofetch";
+          src = lib.cleanSourceWith {
+            src = ./.; # The original, unfiltered source
+            filter = path: type: (lib.hasSuffix "\.proto" path) || (craneLib.filterCargoSources path type);
+          };
+          buildInputs = [
+            pkgs.openssl
+            pkgs.libgit2
+            pkgs.pkg-config
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.Security ];
+          preBuild = ''
+            export HOME=$(mktemp -d)
+          '';
         };
-    });
+      in
+      {
+        packages = rec {
+          inherit protofetch;
+          default = protofetch;
+        };
+        checks = {
+          # Build the crate as part of `nix flake check`
+          inherit protofetch;
+        };
+      }
+    );
 }
-
-
-
-