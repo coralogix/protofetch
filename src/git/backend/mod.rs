9402620a56b68e138540ecb2d2a3cfd72a4ca6e1
Roman Timushev <roman.timushev@coralogix.com>
2026-02-20 22:32:49 +0100

    Preserve semver compatibility

--- src/git/backend/mod.rs
+++ src/git/backend/mod.rs
@@ -3,7 +3,10 @@ pub mod error;
 pub mod libgit2;
 pub mod types;
 
-use std::path::{Path, PathBuf};
+use std::{
+    panic::{RefUnwindSafe, UnwindSafe},
+    path::{Path, PathBuf},
+};
 
 use log::info;
 use serde::Deserialize;
@@ -34,7 +37,7 @@ pub trait GitRepository {
 }
 
 /// Factory for opening or creating git repositories.
-pub trait GitBackend {
+pub trait GitBackend: UnwindSafe + RefUnwindSafe {
     /// Initialize a new bare repository at the given path and return a handle to it.
     fn init_bare(&self, path: &Path) -> Result<Box<dyn GitRepository>, GitBackendError>;
 
